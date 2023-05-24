063135b0b7592b4b64cdc9567c4df6d0c6c31431
Niklas Wieland <niklaswielandhotmail.de>
2023-05-24 13:07:15 +0200

    add fetch function

--- index.js
+++ index.js
@@ -14,14 +14,17 @@ const maxPage = 1;
 const page = 1;
 const searchQuery = "";
 
-createCharacterCard();
 
-async function fetchCharacters(){
+/* createCharacterCard(); */
+
+ async function fetchCharacters(){
 try {
   const response = await fetch("https://rickandmortyapi.com/api/character");
   
   if(response.ok){
   const data = await response.json();
+  console.log(data.results)
+  data.results.forEach((character)=>{createCharacterCard(character.image,"altText",character.name, character.status, character.type, character.episode.length )})  
   return data;
 } else {
   console.error("Bad Response");
@@ -31,3 +34,9 @@ catch(error) {
 console.error("Total Error");
 }
 }
+
+fetchCharacters();
+
+
+
+
